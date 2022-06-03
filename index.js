be70969ecbbad04f19a7db0b405ed3f04238814b
Davide <davide.ceschia@gmail.com>
2022-06-03 16:56:44 +0200

    remove astro from common config

--- index.js
+++ index.js
@@ -387,17 +387,5 @@ module.exports = {
        */
       extends: ["plugin:jest/recommended", "plugin:jest/style"],
     },
-    {
-      // Define the configuration for `.astro` file.
-      files: ["*.astro"],
-      // Allows Astro components to be parsed.
-      parser: "astro-eslint-parser",
-      // Parse the script in `.astro` as TypeScript by adding the following configuration.
-      parserOptions: {
-        parser: "@typescript-eslint/parser",
-        extraFileExtensions: [".astro"],
-      },
-      rules: {},
-    },
   ],
 };
